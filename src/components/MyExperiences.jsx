141589dc7c9caf486bb67afa23eb7c6e8fa2e4be
Raj-kumar-singha <itsrajkumarsingha@gmail.com>
2025-01-24 19:22:34 +0530

    added the about page functionalities

---
+++ src/components/MyExperiences.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyExperiences = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat ea magni eum nam earum exercitationem quis, facere at cum dicta officia. Eos dolorem consequuntur, harum at ut cupiditate ipsum?
+    </div>
+  )
+}
+
+export default MyExperiences
